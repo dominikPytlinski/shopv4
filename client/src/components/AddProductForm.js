dee8ca31decee02bbd5459d64854bdc3de34f84f
unknown <dominik_p@wp.pl>
2019-07-29 19:41:21 +0200

    Crerated add product form

--- client/src/components/AddProductForm.js
+++ client/src/components/AddProductForm.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 
 class AddProductForm extends Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            dropzoneText: "Drag 'n' drop some files here, or click to select files"
+        }
+    }
+
     render() {
         return (
-            <form className="add-product-form">
-
-            </form>
+            <div className="add-product-form">
+                <form>
+                    <div className="form-control">
+                        <label htmlFor="name">Nazwa:</label>
+                        <input type="text" id="name" />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="desc">Opis:</label>
+                        <textarea id="desc"></textarea>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="price">Cena: </label>
+                        <input type="text" id="price" />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="img">Zdjęcie: </label>
+                        <Dropzone
+                            onDrop={acceptedFiles => console.log(acceptedFiles)}
+                        >
+                            {
+                                ({ getRootProps, getInputProps }) => {
+                                    return(
+                                        <section>
+                                            <div {...getRootProps()}>
+                                                <input {...getInputProps()} />
+                                                <p>{this.state.dropzoneText}</p>
+                                            </div>
+                                        </section>
+                                    )
+                                }
+                            }
+                        </Dropzone>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="categoryId">Kategoria</label>
+                        <select id="categoryId">
+                            <option>Wybierz</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <button type="submit" className="btn btn-primary" >Zapisz</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
